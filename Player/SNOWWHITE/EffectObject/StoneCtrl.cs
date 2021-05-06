a6699c8c039a5ac3b27ea0ee7c92e7ddf4f1efbb
ゴ ショウケン(Richard Wu) <richard41407@gmail.com>
2021-05-06 09:56:50 +0900

    Add files via upload

---
+++ Player/SNOWWHITE/EffectObject/StoneCtrl.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StoneCtrl : MonoBehaviour {
+
+	Animator animator;
+
+	[System.NonSerialized] public Transform owner;
+
+    //控制變數
+    public float StoneSpeedX;
+	public float StoneFlyForce;
+	public float StoneGravity;
+	
+
+	//狀態變數
+	public bool isHit = false;
+
+	float dir;
+
+
+
+	void Awake() {
+		animator = GetComponent<Animator>();
+	}
+
+	void Start () {
+		if (!owner) {
+			return;
+		}
+		if (owner != null) {
+			this.transform.localScale = owner.lossyScale;
+			if(owner.lossyScale.x > 0)dir = 1;
+			else dir = -1;
+		}
+
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0.0f, 0.0f);
+		GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0.0f, StoneFlyForce));
+		GetComponent<Rigidbody2D> ().gravityScale = StoneGravity;
+	}
+
+	void FixedUpdate(){
+		if (!isHit) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (StoneSpeedX * dir, GetComponent<Rigidbody2D>().velocity.y);
+
+		}
+		else{
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (0,0);
+			animator.SetBool("isHit",true);
+		}
+	}
+
+	//=======================動畫用程式==============================
+
+	public void AOpenisHit(){
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (0,0);
+		isHit = true;
+		animator.SetBool("isHit",true);
+	}
+
+
+
+}
---
+++ Player/WallCollider.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCollider : MonoBehaviour {
+
+	XXXCtrl character;
+
+	void Awake() {
+		character = transform.parent.GetComponent<XXXCtrl>();
+	}
+
+	//判斷是否貼牆
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Wall") {
+			character.isTouchingWall = true;
+			character.isDashing = false;
+			character.isDashJump = false;
+			character.toSetVelocityX = true;  
+			character.setVelocityX   = 0.0f;  
+		}
+
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "Wall") {
+			character.isTouchingWall = false;
+		}
+	}
+}
